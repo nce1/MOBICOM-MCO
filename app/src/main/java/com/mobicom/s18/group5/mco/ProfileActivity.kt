6e156b2d55f33e944ce28d00dcfb20c27f8a3fc9
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-10-18 22:10:05 +0800

    add microspot and profile

---
+++ app/src/main/java/com/mobicom/s18/group5/mco/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.mobicom.s18.group5.mco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
