6de492115946f00f3b31959b80238d812ed92ad0
nce1 <nce.ioclen@gmail.com>
2025-11-14 16:11:04 +0800

    Clean reset

---
+++ app/src/main/java/com/mobdeve/s18/group5/bayanihanspots/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s18.group5.bayanihanspots.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/mobdeve/s18/group5/bayanihanspots/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s18.group5.bayanihanspots.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
